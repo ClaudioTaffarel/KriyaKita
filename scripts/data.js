cc3d3e929845d3efb1055e0b6f89bddbc9c118c9
ClaudioTaffarel <claudio.taffarel889@gmail.com>
2025-09-25 14:50:36 +0700

    Commit 1 Claudio: Menyelesaikan Home Page dan Navbar

---
+++ scripts/data.js
@@ -0,0 +1,25 @@
+// const destinations = [
+//   {
+//     id: 1,
+//     name: "Danau Toba",
+//     image: "assets/toba.jpg",
+//     description: "Danau vulkanik terbesar di dunia, pemandangan indah.",
+//     price: "Rp 50.000"
+//   },
+//   {
+//     id: 2,
+//     name: "Borobudur",
+//     image: "assets/borobudur.jpg",
+//     description: "Candi Buddha terbesar di dunia.",
+//     price: "Rp 75.000"
+//   },
+//   {
+//     id: 3,
+//     name: "Raja Ampat",
+//     image: "assets/rajaampat.jpg",
+//     description: "Surga bawah laut di Papua Barat.",
+//     price: "Rp 150.000"
+//   }
+// ];
+
+
---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+// const grid = document.getElementById("destGrid");
+
+// destinations.forEach(dest => {
+//     const card = document.createElement("div");
+//     card.classList.add("destCardzz");
+//     card.innerHTML = `
+//         <img src="${dest.image}" alt="${dest.name}">
+//         <h3>${dest.name}</h3>
+//     `;
+//     card.onclick = () => {
+//         window.location.href = `destContz.html?id=${dest.id}`;
+//     };
+//     grid.appendChild(card);
+// });
