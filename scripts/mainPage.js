a53d38ba726a7d25de696ff358fed42d32d5d0a2
ClaudioTaffarel <claudio.taffarel889@gmail.com>
2025-10-07 09:05:49 +0700

    Commit 6 Christopher: Fix Logic Error

--- scripts/mainPage.js
+++ scripts/mainPage.js
@@ -1,4 +1,7 @@
 const colGridz = document.getElementById("colGrid");
+const hamburger = document.querySelector(".hamburgerzz");
+const navMenu = document.querySelector(".navContszz");
+const header = document.querySelector('.headerzz');
 
 koleksiz.slice(0, 9).forEach(itemz => {
     const cardz = document.createElement("div");
@@ -28,5 +31,23 @@ document.addEventListener("DOMContentLoaded", () => {
     const navbarUsername = document.getElementById("navbarUsername");
     if (navbarUsername) {
         navbarUsername.textContent = user.username;
+    }
+});
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".navBtnzz").forEach(link => {
+    link.addEventListener("click", () => {
+        navMenu.classList.remove("active");
+    });
+});
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 50) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
     }
 });
